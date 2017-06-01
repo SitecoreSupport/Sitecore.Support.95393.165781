9fe52cc24cdf11c7827cb3fb8895a79e164b29d0
DK\rbi <beardedmanul@gmail.com>
2017-06-01 17:36:26 +0300

    Commit Patch!

---
+++ src/Sitecore.Support.95393.165781/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95393.165781")]
+[assembly: AssemblyProduct("Sitecore.Support.95393.165781")]
+[assembly: ComVisible(false)]
